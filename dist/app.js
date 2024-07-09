15eba783e8d222eee07b8ed726bb78a1b2bbd047
ksgolu <satyamshay@gmail.com>
2024-07-09 14:49:43 +0530

    fixed dist for deployment

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,12 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const eventRoute_1 = __importDefault(require("./routes/eventRoute"));
+const eventRoute_1 = __importDefault(require("./src/routes/eventRoute"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use("/event", eventRoute_1.default);
